9c13f7f9f355982fb1907044669ba62e0dbe94ca
mariiaz <mariiaz@student.chalmers.se>
2023-09-26 17:14:36 +0200

    add application page and adjustments to Home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
-    <b-jumbotron header="DIT342 Frontend" lead="Welcome to your DIT342 Frontend Vue.js App">
-      <b-button class="btn_message" variant="primary" v-on:click="getMessage()" >Get Message from Server</b-button>
+      <b-button class="btn_message" variant="primary" v-on:click="getMessage()">Get Message from Server</b-button>
       <p>Message from the server:<br/>
       {{ message }}</p>
-    </b-jumbotron>
   </div>
 </template>
 
