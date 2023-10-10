d93f5a0c9a0879ea9f0f5f43fafde2650e88578d
Agrima Singh <agrima@student.chalmers.se>
2023-10-11 00:50:38 +0200

    Refined the UI of the job description page

--- client/src/views/CompanyProfile.vue
+++ client/src/views/CompanyProfile.vue
@@ -4,82 +4,73 @@
     <div class="row">
       <div class="col-md-6 custom-left-align">
         <div>
-          <h2>{{job.title}}</h2>
+          <h2>{{company.name}}</h2>
           <div class="d-flex align-items-center">
-          <h5 class="mr-3 text-muted">at {{job.company_name}}</h5>
-          <h5 class="mr-3 job-enrollment-badge mr-2 mb-2">{{job.job_enrollment_status}}</h5>
+          <h5 class="mr-3">at {{company.email}}</h5>
+          <h5 class="mr-3 job-enrollment-badge mr-2 mb-2">{{company.logo}}</h5>
           <h5 v-if="job.validation" class="custom-alert alert-success rounded">Validated</h5>
           <p v-else class="custom-alert alert-danger rounded">Not Validated</p>
         </div>
         </div>
         <div class="mb-4">
           <h1>Description</h1>
-          <p>{{job.description}}</p>
+          <p>{{company.locations}}</p>
         </div>
         <div>
           <h1>Skills</h1>
           <span
-          v-for="skill in job.skills" :key="skill" class="custom-badge mr-2 mb-2">
-          {{ skill }}
+          v-for="jobs in company.jobs" :key="jobs" class="custom-badge mr-2 mb-2">
+          {{ jobs }}
         </span>
         </div>
       </div>
       <div class="col-md-6">
-        <div class="row">
-  <div class="col-sm-6">
-    <div class="card mb-4">
-      <div class="card-body box text-white rounded">
-        <h5 class="card-title">Salary</h5>
-        <h4 class="text-colour">${{job.yearly_salary_min}} - ${{job.yearly_salary_max}}</h4>
-        <h5 class="text-muted">USD</h5>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-6">
-    <div class="card mb-4">
-      <div class="card-body box text-white rounded">
-        <h5 class="card-title">Job Location</h5>
-        <h5 class="card-text text-colour">{{job.location}}</h5>
-        <h5 class="text-muted">Headquarters</h5>
-      </div>
-    </div>
-  </div>
+        <div class="float-right">
+        <b-button href="#" size="lg" class="applyBtn mb-4">Apply</b-button>
         </div>
+<div class="card text-white box col-md-12 rounded mb-4">
+<div class="row">
+  <div class="col-md-6 d-flex flex-column h-100 justify-content-center align-items-center">
+    <h5 class="card-title">Salary (USD)</h5>
+    <h4 class="salary">${{job.yearly_salary_min}} - ${{job.yearly_salary_max}}</h4>
+    <h5>Yearly Salary</h5>
+  </div>
+  <div class="col-md-6">
+    <h5 class="card-title">Job Location</h5>
+    <p class="card-text">{{job.location}}</p>
+  </div>
+</div>
+</div>
         <div class="row">
-  <div class="col-sm-12 mb-3 mb-4">
+  <div class="col-sm-12 mb-3 mb-sm-0">
     <div class="card custom-blue-border">
       <div class="card-body">
         <h5 class="card-title text-left">Job Overview</h5>
         <div class="row">
         <div class="col-md-4 mb-2">
-        <p class="card-title text-left text-muted">JOB POSTED</p>
+        <p class="card-title text-left">JOB POSTED</p>
         <p class="card-text text-left mb-4" style="font-weight: bold;">{{ formatDate(job.date_posted) }}</p>
         </div>
         <div class="col-md-4 mb-2">
-        <p class="card-title text-left text-muted">JOB EXPIRES IN</p>
+        <p class="card-title text-left">JOB EXPIRES IN</p>
         <p class="card-text text-left" style="font-weight: bold;">{{formatDate(job.deadline)}}</p>
         </div>
         <div class="col-md-4 mb-2">
-        <p class="card-title text-left text-muted">JOB LEVEL</p>
+        <p class="card-title text-left">JOB LEVEL</p>
         <p class="card-text text-left" style="font-weight: bold;">{{job.job_level}}</p>
         </div>
         <div class="col-md-4 mb-2">
-        <p class="card-title text-left text-muted">JOB STATUS</p>
+        <p class="card-title text-left">JOB STATUS</p>
         <p class="card-text text-left" style="font-weight: bold;">{{job.job_enrollment_status}}</p>
         </div>
         <div class="col-md-4 mb-2">
-          <p class="card-title text-left text-muted">EDUCATION</p>
+          <p class="card-title text-left">EDUCATION</p>
         <p class="card-text text-left" style="font-weight: bold;">{{job.education_level}}</p>
         </div>
       </div>
     </div>
     </div>
   </div>
-
-  <div class="col-md-12 mt-4">
-        <b-button href="#" size="lg" class="applyBtn mb-4">Apply</b-button>
-        </div>
-
         </div>
       </div>
     </div>
@@ -91,19 +82,19 @@
 import { Api } from '@/Api'
 
 export default {
-  name: 'JobDescription',
+  name: 'CompanyProfile',
   data() {
     return {
       job: {}
     }
   },
   created() {
-    this.fetchJob()
+    this.fetchCompany()
   },
   methods: {
-    fetchJob() {
-      const jobId = this.$route.params.id
-      Api.get(`/v1/jobs/${jobId}`)
+    fetchCompany() {
+      const companyId = this.$route.params.id
+      Api.get(`/v1/companies/${companyId}`)
         .then(response => {
           this.job = response.data
           console.log(this.job)
@@ -142,14 +133,20 @@ export default {
     height: 56px;
 }
 .custom-blue-border{
-  border: 2px solid rgb(11, 63, 234);
+  border: 2px solid rgb(7, 25, 82, 1);
 }
-.text-colour{
+.salary{
   color: aquamarine;
 }
 .box{
   background-color:rgba(7, 25, 82, 1);
   height: 150px;
+}
+.vertical-divider {
+  border-left: 2px solid white;
+  height: 100%;
+  margin: 0 auto;
+  padding: 0;
 }
 .custom-alert {
   width: 110px;
