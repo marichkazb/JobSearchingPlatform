48b50cdd707d4f01ffc5d5c7948cfd59aaf300bf
Agrima Singh <agrima@student.chalmers.se>
2023-10-10 17:08:19 +0200

    Add all the features and functioanlities of job description page'

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import JobDescription from './views/JobDescription'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/job-description/:id',
+      name: 'job-description',
+      component: JobDescription
     }
   ]
 })
