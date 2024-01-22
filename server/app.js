8aadaf7447704742ab516062272150aebed54c44
mariiaz <mariiaz@student.chalmers.se>
2024-01-22 19:33:51 +0100

    extract important url information to the .env. Todo: refactor correct path

--- server/app.js
+++ server/app.js
@@ -17,10 +17,8 @@ const candidateRoutes = require("./routes/candidateRoutes");
 
 const { authenticate, classifyRequester } = require("./authMiddleware");
 
-const productionMongoURI =
-  "mongodb+srv://admin:admin1234@cluster0.0yuyemj.mongodb.net/?retryWrites=true&w=majority";
 const testMongoURI = "mongodb://127.0.0.1:27017/serverTestDB"; // used for manual postman testing without interference on the online database.
-const mongoURI = process.env.MONGODB_URI || productionMongoURI;
+const mongoURI = process.env.MONGODB_URI || process.env.VUE_APP_PRODUCTION_MONGO_URI;
 
 const productionPort = 3000;
 const port = process.env.PORT || productionPort;
